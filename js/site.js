37d131bad54ab9744e15f26c5784bf537ef57b42
Rex Lundstrom <rexlundstrom@gmail.com>
2023-04-14 17:35:38 -0400

    functionality and beauty! info is up to date

--- js/site.js
+++ js/site.js
@@ -5,14 +5,15 @@ const getValues = () => {
   // send it through the reversed string function
   const palindromeInfoArray = checkForPalindrome(userInput);
 
-  displayString(palindromeInfoArray);
+  displayResults(palindromeInfoArray);
 };
 
 // business logic
 const checkForPalindrome = (message) => {
   // declare empty string
+  if (message == "") message = `Nothing at all`;
   let reversedStr = "";
-  let inputStr = message.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  let inputStr = message.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
   const output = [message];
 
   for (let i = inputStr.length - 1; i >= 0; i--) {
@@ -23,18 +24,22 @@ const checkForPalindrome = (message) => {
 };
 
 // view function
-const displayString = (array) => {
+const displayResults = (array) => {
   // display string on the page
   if (array[2] == true) {
     // congrats message
-  document.getElementById("failure-alert").classList.add("d-none");
-  document.getElementById("success-message").textContent = `${array[0]} written backwards is ${array[1]}! You've got yourself a palindrome.`;
-  document.getElementById("success-alert").classList.remove("d-none");
+    document.getElementById("failure-alert").classList.add("d-none");
+    document.getElementById(
+      "success-message"
+    ).textContent = `"${array[0]}" written backwards is ${array[1]}! You've got yourself a palindrome.`;
+    document.getElementById("success-alert").classList.remove("d-none");
   } else {
     // too bad message
-  document.getElementById("success-alert").classList.add("d-none");
-  document.getElementById("failure-message").textContent = `${array[0]} written backwards is ${array[1]} which is not a palindrome.`;
-  document.getElementById("failure-alert").classList.remove("d-none");
+    document.getElementById("success-alert").classList.add("d-none");
+    document.getElementById(
+      "failure-message"
+    ).textContent = `${array[0]} written backwards is ${array[1]} which is not a palindrome.`;
+    document.getElementById("failure-alert").classList.remove("d-none");
   }
 };
 
